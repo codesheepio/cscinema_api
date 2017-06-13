22112daa64934c1dcdbc4ef4c8ea87f6b568ad90
Arnupharp Viratanapanu <arnupharp@larngeartech.com>
2017-06-14 02:43:59 +0700

    Select bookings with showtime

--- common/models/showtime.js
+++ common/models/showtime.js
@@ -3,17 +3,17 @@
 module.exports = function(Showtime) {
   Showtime.on('attached', function() {
     // Also include movies in return json
-    var findById = Showtime.findById
     Showtime.findById = function(id, filter, cb) {
       var newFilter = filter || {}
-      newFilter.include = 'movie'
-      return findById.call(this, id, newFilter, cb)
+      newFilter.id = id
+      newFilter.include = ['movie', 'bookings']
+      return Showtime.find.call(this, newFilter, cb)
     }
 
     var find = Showtime.find
     Showtime.find = function(filter, cb) {
       var newFilter = filter || {}
-      newFilter.include = 'movie'
+      newFilter.include = ['movie', 'bookings']
       return find.call(this, newFilter, cb)
     }
   })
