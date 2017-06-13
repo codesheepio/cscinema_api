0c8315649bc9d8eb27c162dcc0303422ddc48dc2
Arnupharp Viratanapanu <arnupharp@larngeartech.com>
2017-06-14 01:22:50 +0700

    Include movie along with showtime

--- common/models/showtime.js
+++ common/models/showtime.js
@@ -1,3 +1,20 @@
 'use strict'
 
-module.exports = function(Showtime) {}
+module.exports = function(Showtime) {
+  Showtime.on('attached', function() {
+    // Also include movies in return json
+    var findById = Showtime.findById
+    Showtime.findById = function(id, filter, cb) {
+      var newFilter = filter || {}
+      newFilter.include = 'movie'
+      return findById.call(this, id, newFilter, cb)
+    }
+
+    var find = Showtime.find
+    Showtime.find = function(filter, cb) {
+      var newFilter = filter || {}
+      newFilter.include = 'movie'
+      return find.call(this, newFilter, cb)
+    }
+  })
+}
