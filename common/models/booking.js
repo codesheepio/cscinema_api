dbed0e7d4db75fd2a4c3d5d6980599bb65ec12a1
Arnupharp Viratanapanu <arnupharp@larngeartech.com>
2017-06-13 22:15:11 +0700

    Find booking by code instead of id

--- common/models/booking.js
+++ common/models/booking.js
@@ -12,7 +12,18 @@ function genCode(length) {
 }
 
 module.exports = function(Booking) {
+  // Override findById function to find by code instead of id
+  Booking.on('attached', function() {
+    Booking.findById = function(id, filter, cb) {
+      var newFilter = filter || {}
+      newFilter.code = id
+      return Booking.find.call(this, newFilter, cb)
+    }
+  })
+
+  // Generate code before save
   Booking.observe('before save', function generateCode(ctx, next) {
+    // If model is created not update
     if (ctx.isNewInstance && ctx.instance) {
       ctx.instance.code = genCode(4)
     }
