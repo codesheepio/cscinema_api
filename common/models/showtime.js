9e557232e13906eec26ca13be66c86614dd405f7
Arnupharp Viratanapanu <arnupharp@larngeartech.com>
2017-06-15 00:20:38 +0700

    Add showtimes/theatres and showtimes/movies and fix bug

--- common/models/showtime.js
+++ common/models/showtime.js
@@ -5,7 +5,7 @@ module.exports = function(Showtime) {
     // Also include movies in return json
     Showtime.findById = function(id, filter, cb) {
       var newFilter = filter || {}
-      newFilter.id = id
+      newFilter.where = { id: id }
       newFilter.include = ['movie', 'bookings']
       return Showtime.find.call(this, newFilter, cb)
     }
@@ -16,5 +16,25 @@ module.exports = function(Showtime) {
       newFilter.include = ['movie', 'bookings']
       return find.call(this, newFilter, cb)
     }
+  })
+
+  // Find by theatreId
+  Showtime.theatres = function(theatreId, cb) {
+    return Showtime.find.call(this, { where: { theatre: theatreId } }, cb)
+  }
+  Showtime.remoteMethod('theatres', {
+    http: { verb: 'get', path: '/theatres/:theatreId' },
+    accepts: { arg: 'theatreId', type: 'number', required: true },
+    returns: { type: 'array', root: true },
+  })
+
+  // Find by movieId
+  Showtime.movies = function(movieId, cb) {
+    return Showtime.find.call(this, { where: { movie_id: movieId } }, cb)
+  }
+  Showtime.remoteMethod('movies', {
+    http: { verb: 'get', path: '/movies/:movieId' },
+    accepts: { arg: 'movieId', type: 'number', required: true },
+    returns: { type: 'array', root: true },
   })
 }
