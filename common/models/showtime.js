8eb242d076479e0ed6d0ed0ebd604c9624454eb7
Arnupharp Viratanapanu <arnupharp@larngeartech.com>
2017-06-17 19:53:33 +0700

    Return movie detail with showtime

--- common/models/showtime.js
+++ common/models/showtime.js
@@ -1,6 +1,32 @@
 'use strict'
 
 module.exports = function(Showtime) {
+  Showtime.on('attached', function() {
+    var findById = Showtime.findById
+    Showtime.findById = function(id, filter, options, cb) {
+      var newFilter = {
+        ...filter,
+      }
+      if (newFilter.include === undefined) {
+        newFilter.include = ['movie']
+      } else {
+        newFilter.include = ['movie', ...filter.include]
+      }
+      return findById.call(this, id, newFilter, options, cb)
+    }
+    var find = Showtime.find
+    Showtime.find = function(filter, options, cb) {
+      var newFilter = {
+        ...filter,
+      }
+      if (newFilter.include === undefined) {
+        newFilter.include = ['movie']
+      } else {
+        newFilter.include = ['movie', ...filter.include]
+      }
+      return find.call(this, newFilter, options, cb)
+    }
+  })
   // Find by theatreId
   Showtime.theatres = function(theatreId, cb) {
     return Showtime.find.call(this, { where: { theatre: theatreId } }, cb)
